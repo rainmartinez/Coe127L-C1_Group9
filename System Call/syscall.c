2c960c82e21e49de8ecdc12b57bc442a94120757
Rod Arnold Labarete <42694306+SpaceGopnik@users.noreply.github.com>
2018-09-23 21:59:11 +0800

    Add files via upload

--- System Call/syscall.c
+++ System Call/syscall.c
@@ -4,6 +4,24 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+
+struct file_descriptor
+{
+  int fd_num;
+  tid_t owner;
+  struct file *file_struct;
+  struct list_elem elem;
+};
+
+/* a list of open files, represents all the files open by the user process
+   through syscalls. */
+struct list open_files; 
+
+/* the lock used by syscalls involving file system to ensure only one thread at
+ * a time is accessing file system 
+ */
+struct lock fs_lock;
+
 static void syscall_handler (struct intr_frame *);
 
 void
