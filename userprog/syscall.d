ca7bf52e2ef36d9e98d04c281c97b7096dc830aa
Rod Arnold Labarete <42694306+SpaceGopnik@users.noreply.github.com>
2018-10-16 17:09:33 +0800

    Add files via upload

---
+++ userprog/syscall.d
@@ -0,0 +1,11 @@
+userprog/syscall.o: ../../userprog/syscall.c ../../devices/shutdown.h \
+ ../../lib/debug.h ../../devices/input.h ../../lib/stdbool.h \
+ ../../lib/stdint.h ../../userprog/syscall.h ../../userprog/process.h \
+ ../../threads/thread.h ../../lib/kernel/list.h ../../lib/stddef.h \
+ ../../threads/synch.h ../../filesys/filesys.h ../../filesys/off_t.h \
+ ../../filesys/file.h ../../filesys/inode.h ../../devices/block.h \
+ ../../lib/inttypes.h ../../filesys/directory.h ../../threads/palloc.h \
+ ../../threads/malloc.h ../../lib/stdio.h ../../lib/stdarg.h \
+ ../../lib/kernel/stdio.h ../../lib/syscall-nr.h \
+ ../../threads/interrupt.h ../../threads/vaddr.h ../../threads/loader.h \
+ ../../lib/kernel/list.h
